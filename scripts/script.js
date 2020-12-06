4edf20bb9d8b1eeb5b2ba6a3e359da7da3c26042
Mina B <mbui14@my.bcit.ca>
2020-12-06 14:21:26 -0800

    notes on script file

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,25 @@
 //  date: 2020-11-24
 //  title: twd - javascript game - js
 
-//
+/*      javascript game
+
+    basics: 
+        - character on the bottom, controlled by directional keys
+        - enemies that are going from the top of the screen to the bottom
+            - when enemies run into the enemy, the game is over
+
+    - goal:   
+            - avoid enemies and collect coins
+            - last as long as you can (like flappy bird?)
+
+    - have a timer function
+            - speed of enemies falling becomes faster with time
+
+    - user interactions:
+        - collect coins that are also falling ==> counts how many coins you run into?
+        - avoid objects with directional keys
+        - have the game end when it runs into the enemy
+    
+  
+  
+  */
