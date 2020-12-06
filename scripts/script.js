c58e3a67e4ee2d003b0ad5bd5f5a8fba888cdeac
Mina B <mbui14@my.bcit.ca>
2020-12-06 14:24:54 -0800

    done note taking

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,23 @@
 //  date: 2020-11-24
 //  title: twd - javascript game - js
 
-/*      javascript game
+/*      javascript game - basically frogger lol
 
     basics: 
         - character on the bottom, controlled by left and right directional keys
         - enemies that are going from the top of the screen to the bottom
-            - when enemies run into the user, the game is over (show "game over" screen)
+                => when enemies run into user, show "game over" screen
+        - have a timer function
+                => speed of enemies falling becomes faster with time
 
-    - goal:   
-            - avoid enemies and collect coins
-            - last as long as you can (like flappy bird?)
+    goal:   
+        - avoid enemies and collect coins
+        - last as long as you can (like flappy bird?)
 
-    - have a timer function
-            - speed of enemies falling becomes faster with time
-
-    - user interactions:
-        - collect coins that are also falling ==> counts how many coins you run into?
+    user interactions:
+        - collect coins that are also falling 
+                => counts how many coins you run into?
         - avoid objects with directional keys
         - have the game end when it runs into the enemy
     
-  
-  
   */
