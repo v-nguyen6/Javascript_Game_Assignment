318f592d1bf710b61cfa182a7ebb5e23adb5489c
Mina B <mbui14@my.bcit.ca>
2020-12-06 15:01:45 -0800

    notes, figuring out how game work
    lol

--- scripts/script.js
+++ scripts/script.js
@@ -2,30 +2,102 @@
 //  date: 2020-11-24
 //  title: twd - javascript game - js
 
-/*      javascript game - basically frogger lol
+// --------------------------------------------------------------------------------------
+/*      javascript game - basically dino frogger lol
 
     basics: 
-        - character on the bottom, controlled by left and right directional keys
-        - enemies that are going from the top of the screen to the bottom
+        - user's dinosaur on the bottom, controlled by left and right directional keys
+        - enemies (i.e. meteors) that are going from the top of the screen to the bottom
                 => when enemies run into user, show "game over" screen
         - have a timer function
                 => speed of enemies falling becomes faster with time
 
+
     goal:   
-        - avoid enemies and collect coins
+        - avoid enemies and collect meat
         - last as long as you can (like flappy bird?)
 
     user interactions:
-        - collect coins that are also falling 
-                => counts how many coins you run into?
-        - avoid objects with directional keys
-        - have the game end when it runs into the enemy
-    
-  */
+        - collect meat that are also falling 
+                => counts how much meat you run into?
+        - avoid enemies with directional keys
+        - have the game end when dinosaur is in contact with the meteor
 
-  /*
+    game progression
+        - [game start]
+            - "press enter to continue"
 
-    questions to ask michael lol:
+        - [character selection screen]
+            - "left and right to select a character, press enter to select"
+
+        - [game begins]
+            - meteors immediately begins falling (starts off slow)
+            - meat begins falling (user can collect for points)
+
+        - [game over]
+            - only when user runs into the meteor
+            - shows "game over"
+                - shows your stats => (1) how long survived and (2) how many points collected
+
+    *********questions to ask michael lol:
         - does the game need to be playable on mobile?
-
+        - how to reset the game after [game over]?
   */
+// ------------------------------- the actual code lol ------------------------------- 
+
+
+// [character selection]
+// 
+/*
+    rules:
+        1. left and right to select a character, press enter to select
+        2. when user selects dinosaur, it changes the img for the rest of the game
+*/      
+
+
+
+
+
+// --------------------------------------------------------------------------------------
+// [user => dinosaurs]
+// 
+/*
+    rules:
+        1. move left and right on screen
+            only within .game-container
+*/      
+
+
+
+
+
+// --------------------------------------------------------------------------------------
+// [enemies => meteors]
+/*
+    rules:
+        1. move automatically top to bottom
+        2. falls faster with time
+        3. when meteors come in contact with dinosaur, game over
+ */
+
+
+
+
+// --------------------------------------------------------------------------------------
+// [timer function]
+/*
+    rules:
+        1. 
+*/
+
+
+
+
+// --------------------------------------------------------------------------------------
+// [earn points => collect falling meat]
+/*
+    rules:
+        1. 
+*/
+
+ 
